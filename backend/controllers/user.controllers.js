cf50c2bf31533ed9739b73dd91211cc66a4cc180
Dipesh Kafle <dipeshkaflen2060@gmail.com>
2025-03-14 19:53:35 +0530

    Add Login backend!!

--- backend/controllers/user.controllers.js
+++ backend/controllers/user.controllers.js
@@ -1,10 +1,12 @@
 const { User } = require("../model/user.model");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
+//register
 const handleUserRegister = async (req, res) => {
   try {
     const { username, email, password } = req.body;
-    const user = await User.find({ username: username });
+    const user = await User.findOne({ email: email });
     if (user) {
       return res
         .status(409)
@@ -18,7 +20,46 @@ const handleUserRegister = async (req, res) => {
     res.status(500).json({ status: false, msg: "Internal server error" });
   }
 };
-const handleUserLogin = async (req, res) => {};
+
+//for login
+const handleUserLogin = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const userDetails = await User.findOne({ email: email });
+
+    if (!userDetails) {
+      return res
+        .status(404)
+        .json({ status: false, msg: "User doesn't exists" });
+    }
+    const isPassEqual = bcrypt.compare(password, userDetails.password);
+    if (!isPassEqual) {
+      return res
+        .status(401)
+        .json({ status: false, msg: "Password doesn't matched" });
+    }
+
+    const jwtToken = jwt.sign(
+      {
+        username: userDetails.username,
+        email: email,
+      },
+      process.env.JWT_SECRET,
+      {
+        expiresIn: "5h",
+      }
+    );
+
+    res.status(200).json({
+      status: true,
+      msg: "User logged in",
+      user: { username: userDetails.username, email, jwtToken },
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ status: false, msg: "Internal server error" });
+  }
+};
 const handleUserLogout = () => {};
 
 module.exports = {
